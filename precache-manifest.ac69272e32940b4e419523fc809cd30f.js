e089ee03f0006a1e97c0d5440679e1e425fe7d08
Nadine Hernandez <nrhernandez93@gmail.com>
2019-01-15 23:29:06 -0500

    Updates

---
+++ precache-manifest.ac69272e32940b4e419523fc809cd30f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "61fcac69a4c0d27c7cc11fec4664299e",
+    "url": "/flower-shuffle/static/media/daffodil.61fcac69.jpg"
+  },
+  {
+    "revision": "99ea74d1c5f2af3bb8c3",
+    "url": "/flower-shuffle/static/css/main.868084cd.chunk.css"
+  },
+  {
+    "revision": "c206b149c0351193f91e",
+    "url": "/flower-shuffle/static/js/1.c206b149.chunk.js"
+  },
+  {
+    "revision": "04773a8720aa10ade5da",
+    "url": "/flower-shuffle/static/js/runtime~main.04773a87.js"
+  },
+  {
+    "revision": "c7284e9165a131f466ae0c8e1300f419",
+    "url": "/flower-shuffle/static/media/dahlia.c7284e91.jpg"
+  },
+  {
+    "revision": "7ba5c5ef17fce7c895e3c00bd78d383b",
+    "url": "/flower-shuffle/static/media/peony.7ba5c5ef.jpg"
+  },
+  {
+    "revision": "b00401c38f107aa85c4fcd189a01b8cc",
+    "url": "/flower-shuffle/static/media/daisy.b00401c3.jpg"
+  },
+  {
+    "revision": "b42b7e55783e99e09ff3a458786ae859",
+    "url": "/flower-shuffle/static/media/carnation.b42b7e55.jpg"
+  },
+  {
+    "revision": "99ea74d1c5f2af3bb8c3",
+    "url": "/flower-shuffle/static/js/main.99ea74d1.chunk.js"
+  },
+  {
+    "revision": "f2bdd50e2ada2c8647ce07e43245ec95",
+    "url": "/flower-shuffle/static/media/gardenia.f2bdd50e.jpg"
+  },
+  {
+    "revision": "bbaf98c4d37994bb721d1be7b9774f2e",
+    "url": "/flower-shuffle/static/media/hydrangea.bbaf98c4.jpg"
+  },
+  {
+    "revision": "8fad520619e2b13ebeb6391a6316ba30",
+    "url": "/flower-shuffle/static/media/lily.8fad5206.jpg"
+  },
+  {
+    "revision": "c7bf75b0f25c199447d250cb6ea4d5e0",
+    "url": "/flower-shuffle/static/media/orchid.c7bf75b0.jpg"
+  },
+  {
+    "revision": "99220c4f0605dae967372c1f221044f9",
+    "url": "/flower-shuffle/static/media/rose.99220c4f.jpg"
+  },
+  {
+    "revision": "ce9e272d07ec5bc968dfd6d35f207218",
+    "url": "/flower-shuffle/static/media/zinnia.ce9e272d.jpg"
+  },
+  {
+    "revision": "3616a200dc018aa25b26cc1899299bf3",
+    "url": "/flower-shuffle/index.html"
+  }
+];
